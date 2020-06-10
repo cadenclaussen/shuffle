8276c3a47621b3bff0c586ee2e7c68bd2ec2bf8f
Caden Claussen <caden.claussen@gmail.com>
2020-06-10 13:44:27 -0700

    made deck and shuffled

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import shuffle
 
-bong = ["bung", "bing", "ding", "ting"]
-newList = shuffle.shuffle(bong)
-print(newList)
+cards = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"]
+suits = ["clubs", "dimonds", "spades", "hearts"]
+deck = []
+
+for suit in suits:
+    for card in cards:
+        addCard = card + " " + suit
+        addCard = str(addCard)
+        deck.append(addCard)    
+print(deck)    
+
+print()
+print("this is the shuffled deck: ")
+print()
+newDeck = shuffle.shuffle(deck)
+print(newDeck)
